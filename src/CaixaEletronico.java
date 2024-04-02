d76749032cf22a2253909b8fe1af0e00b0e09117
Eliane <eliane@fiap.com.br>
2024-04-02 18:59:05 -0300

    Caixa

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CaixaEletronico {
+    public static void menu(){
+        System.out.println("Bem vindo ao Banco FIAP");
+
+        System.out.println("\nOperações disponíveis neste caixa:" +
+                "\n\n1 - Depósito\t\t2 - Saque" +
+                "\n3 - Consultar Saldo\t4 - Sair");
+        System.out.println("Opção: ");
+    }
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int opcao;
+        double saldo = 0, valor;
+        do{
+            menu();
+            opcao = leitor.nextInt();
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: R$ " + String.format("%.2f", saldo));
+                    break;
+                case 4:
+                    System.out.println("Obrigada por utilizar nosso banco!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }while (opcao != 4);
+
+    }
+}
